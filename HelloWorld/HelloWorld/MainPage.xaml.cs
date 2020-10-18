6a48cd195ecf7ed1689fa54324d788c8743d2832
Aditya Oberai <adityaoberai1@gmail.com>
2020-10-19 03:05:37 +0530

    Pushed the entire app

---
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void click_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You have clicked {count} times!";
+        }
+    }
+}
